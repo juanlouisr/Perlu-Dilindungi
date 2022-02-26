14cb1e45086ef5d6135464441fe4a2d41198a114
jordaniel29 <jordandj2001@gmail.com>
2022-02-26 16:02:31 +0700

    ADD sqlitehelper

---
+++ app/src/main/java/com/example/perludilindungi/models/SQLiteHelper.kt
@@ -0,0 +1,62 @@
+package com.example.perludilindungi.models
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+
+object FeedReaderContract {
+    // Table contents are grouped together in an anonymous object.
+    object FeedEntry : BaseColumns {
+        const val TABLE_NAME = "faskes"
+        const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_KODE = "kode"
+        const val COLUMN_NAME_NAMA = "nama"
+        const val COLUMN_NAME_KOTA = "kota"
+        const val COLUMN_NAME_PROVINSI = "provinsi"
+        const val COLUMN_NAME_ALAMAT = "alamat"
+        const val COLUMN_NAME_LATITUDE = "latitude"
+        const val COLUMN_NAME_LONGITUDE = "longitude"
+        const val COLUMN_NAME_TELP = "telp"
+        const val COLUMN_NAME_JENIS_VASKES = "jenis_vaskes"
+        const val COLUMN_NAME_KELAS_RS = "kelas_rs"
+        const val COLUMN_NAME_STATUS = "status"
+        const val COLUMN_NAME_SOURCE_DATA = "source_data"
+    }
+}
+
+private const val SQL_CREATE_ENTRIES =
+    "CREATE TABLE ${FeedReaderContract.FeedEntry.TABLE_NAME} (" +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_ID} INTEGER PRIMARY KEY," +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_KODE} TEXT," +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_NAMA} TEXT)" +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_KOTA} TEXT)" +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_PROVINSI} TEXT)" +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_ALAMAT} TEXT)" +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_LATITUDE} TEXT)" +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_LONGITUDE} TEXT)" +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_TELP} TEXT)" +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_JENIS_VASKES} TEXT)" +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_KELAS_RS} TEXT)" +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_STATUS} TEXT)" +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_SOURCE_DATA} TEXT)"
+
+private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${FeedReaderContract.FeedEntry.TABLE_NAME}"
+
+class SQLiteHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SQL_CREATE_ENTRIES)
+    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL(SQL_DELETE_ENTRIES)
+        onCreate(db)
+    }
+    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        onUpgrade(db, oldVersion, newVersion)
+    }
+
+    companion object {
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "Bookmark.db"
+    }
+}
--- app/src/main/java/com/example/perludilindungi/ui/detailfaskes/DetailFaskesFragment.kt
+++ app/src/main/java/com/example/perludilindungi/ui/detailfaskes/DetailFaskesFragment.kt
@@ -1,15 +1,17 @@
 package com.example.perludilindungi.ui.detailfaskes
 
+import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.perludilindungi.R
 import com.example.perludilindungi.databinding.FragmentDetailFaskesBinding
-import android.content.pm.PackageManager
+import com.example.perludilindungi.models.FeedReaderContract
+import com.example.perludilindungi.models.SQLiteHelper
 
 
 class DetailFaskesFragment : Fragment() {
@@ -21,7 +23,7 @@ class DetailFaskesFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentDetailFaskesBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -29,15 +31,6 @@ class DetailFaskesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Masukkin data dari daftar faskes
-        val nama_faskes = "KLINIK DPR RI"
-        val kode = "N0001702"
-        val jenis_faskes = "KLINIK"
-        val alamat =
-            "Gedung DPR-RI, JL. Gatot Subroto, RT.1/RW.3, Gelora, Kecamatan Tanah Abang, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10270, Indonesia"
-        val no_telp = "(021) 5715801"
-        val status = "Siap Vaksinasi"
-        val latitude = "-6.2101653"
-        val longitude = "106.8004706"
 
         binding.namaFaskes.text = nama_faskes
         binding.noKode.text = kode
@@ -48,20 +41,120 @@ class DetailFaskesFragment : Fragment() {
         if (status != "Siap Vaksinasi") {
             binding.statusImage.setImageResource(R.drawable.ic_resource_false)
         }
+//        if (!dataNotInDB()) {
+//            binding.bookmark.text = "- UnBookmark"
+//        }
 
         //Google Map
-        val gmmIntentUri = Uri.parse("geo:${latitude},${longitude}?q=${nama_faskes}")
-        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
-        mapIntent.setPackage("com.google.android.apps.maps")
         binding.googleMap.setOnClickListener {
-            startActivity(mapIntent)
+            intentToGoogleMaps()
         }
 
-        //Bookmark
+//        //Bookmark
+//        binding.bookmark.setOnClickListener {
+//            if (binding.bookmark.text == "+ Bookmark") {
+//                insertToDB()
+//                binding.bookmark.text = "- Bookmark"
+//            } else {
+//                deleteFromDB()
+//                binding.bookmark.text = "+ Bookmark"
+//            }
+//        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun intentToGoogleMaps() {
+        val gmmIntentUri = Uri.parse("geo:${latitude},${longitude}?q=${nama_faskes}")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        startActivity(mapIntent)
+    }
+
+    private fun dataNotInDB(): Boolean {
+        val dbHelper = this.activity?.let { SQLiteHelper(it.applicationContext) }
+        val db = dbHelper?.readableDatabase
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        val projection = arrayOf(FeedReaderContract.FeedEntry.COLUMN_NAME_ID)
+
+        // Filter results WHERE "title" = 'My Title'
+        val selection = "${FeedReaderContract.FeedEntry.COLUMN_NAME_ID} = ?"
+        val selectionArgs = arrayOf(id.toString())
+
+        val cursor = db?.query(
+            FeedReaderContract.FeedEntry.TABLE_NAME,   // The table to query
+            projection,                 // The array of columns to return (pass null to get all)
+            selection,                  // The columns for the WHERE clause
+            selectionArgs,              // The values for the WHERE clause
+            null,               // don't group the rows
+            null,                // don't filter by row groups
+            null                // The sort order
+        )
+
+        return cursor == null;
+    }
+
+    private fun deleteFromDB() {
+        val dbHelper = this.activity?.let { SQLiteHelper(it.applicationContext) }
+        val db = dbHelper?.readableDatabase
+        // Define 'where' part of query.
+        val selection = "${FeedReaderContract.FeedEntry.COLUMN_NAME_ID} LIKE ?"
+        // Specify arguments in placeholder order.
+        val selectionArgs = arrayOf(id.toString())
+        // Issue SQL statement.
+        db?.delete(FeedReaderContract.FeedEntry.TABLE_NAME, selection, selectionArgs)
+    }
+
+    private fun insertToDB() {
+        val dbHelper = this.activity?.let { SQLiteHelper(it.applicationContext) }
+        val db = dbHelper?.writableDatabase
+
+        val values = ContentValues().apply {
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_ID, Companion.id)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_KODE, kode)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_NAMA, nama_faskes)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_KOTA, kota)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_PROVINSI, provinsi)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_ALAMAT, alamat)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_LATITUDE, latitude)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_LONGITUDE, longitude)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_TELP, no_telp)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_JENIS_VASKES, jenis_faskes)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_KELAS_RS, kelas_rs)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_STATUS, status)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_SOURCE_DATA, source_data)
+        }
+
+        db?.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values)
+    }
+
+    override fun onDestroy() {
+        val dbHelper = this.activity?.let { SQLiteHelper(it.applicationContext) }
+        dbHelper?.close()
+        super.onDestroy()
+    }
+
+    companion object {
+        private const val id = 2725
+        private const val kota = "KOTA ADM. JAKARTA PUSAT"
+        private const val provinsi = "DKI JAKARTA"
+        private const val kelas_rs = "1"
+        private const val source_data = "Control Tower KPCPEN"
+        private const val nama_faskes = "KLINIK DPR RI"
+        private const val kode = "N0001702"
+        private const val jenis_faskes = "KLINIK"
+        private const val alamat =
+            "Gedung DPR-RI, JL. Gatot Subroto, RT.1/RW.3, Gelora, Kecamatan Tanah Abang, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10270, Indonesia"
+        private const val no_telp = "(021) 5715801"
+        private const val status = "Siap Vaksinasi"
+        private const val latitude = "-6.2101653"
+        private const val longitude = "106.8004706"
+    }
 }
+
