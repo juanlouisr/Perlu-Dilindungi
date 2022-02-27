6ac7d68628c230b4e9c5300b6c65f93c4511ef7e
nabilahann <13519097@std.stei.itb.ac.id>
2022-02-27 16:03:47 +0700

    add landing page

---
+++ app/src/main/java/com/example/perludilindungi/ui/landingpage/LandingPage.kt
@@ -0,0 +1,31 @@
+package com.example.perludilindungi.ui.landingpage
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.example.perludilindungi.MainActivity
+import com.example.perludilindungi.R
+
+class LandingPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
