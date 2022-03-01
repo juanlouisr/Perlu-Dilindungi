ae89929434ff7bbf5614be13173fa08e821b2015
mizuday <mizuday5.7@gmail.com>
2022-03-01 14:27:14 +0700

    hide action bar

--- app/src/main/java/com/example/perludilindungi/MainActivity.kt
+++ app/src/main/java/com/example/perludilindungi/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity(){
         binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide();
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_bottom_navigation)
         // Passing each menu ID as a set of Ids because each
