ab044ef48870dc644fa15f055620b6fe2caef44f
mizuday <mizuday5.7@gmail.com>
2022-03-01 19:58:53 +0700

    fragment faskes refactoring

--- app/src/main/java/com/example/perludilindungi/ui/faskes/FaskesFragment.kt
+++ app/src/main/java/com/example/perludilindungi/ui/faskes/FaskesFragment.kt
@@ -1,19 +1,26 @@
 package com.example.perludilindungi.ui.faskes
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context.LOCATION_SERVICE
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.example.perludilindungi.R
 import com.example.perludilindungi.databinding.FragmentFaskesBinding
+import com.example.perludilindungi.models.ProvinceResponse
 import com.example.perludilindungi.repository.Repository
 import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.fragment_faskes.*
@@ -33,14 +40,14 @@ class FaskesFragment : Fragment(R.layout.fragment_faskes) {
 
     override fun onResume() {
         super.onResume()
-//        getLastLocation()
-        fetchLoc()
+        getNewLocation()
+        getLastLocation()
+//        fetchLoc()
         Log.d("Debug", viewModel.latitude.toString())
         Log.d("Debug", viewModel.longitude.toString())
         if (binding.autoCompleteTextViewProvince.text == null || binding
                 .autoCompleteTextViewProvince.text.toString() == ""
         ) return
-//        val city = binding.autoCompleteTextViewCity.text.toString()
         viewModel.getCity(binding.autoCompleteTextViewProvince.text.toString())
         viewModel.cityResponse.observe(viewLifecycleOwner) { response ->
             viewModel.cities.clear()
@@ -78,51 +85,19 @@ class FaskesFragment : Fragment(R.layout.fragment_faskes) {
 
         viewModel.getProvince()
         viewModel.provResponse.observe(viewLifecycleOwner) { response ->
-            viewModel.provinces.addAll(response.results.map { it.key }
-                .toList())
-            val arrayAdapter = ArrayAdapter(
-                requireContext(), R.layout
-                    .text_item, viewModel.provinces
-            )
-            binding.autoCompleteTextViewProvince.setAdapter(arrayAdapter)
+            fetchProvince(response)
         }
 
         binding.autoCompleteTextViewProvince.setOnItemClickListener { adapterView, view, i, l ->
-            viewModel.getCity(binding.autoCompleteTextViewProvince.text.toString())
-            viewModel.cityResponse.observe(viewLifecycleOwner) { response ->
-                viewModel.cities.clear()
-                viewModel.cities.addAll(response.results.map { it.key }
-                    .toList())
-                val arrayAdapter = ArrayAdapter(
-                    requireContext(), R.layout
-                        .text_item, viewModel.cities
-                )
-                Log.d(
-                    "Response", response.results.toString()
-                )
-                binding.autoCompleteTextViewCity.setAdapter(arrayAdapter)
-                binding.autoCompleteTextViewCity.text.clear()
-            }
+            fetchCity()
         }
 
         binding.autoCompleteTextViewCity.setOnItemClickListener { adapterView, view, i, l ->
-            val prov: String =
-                binding.autoCompleteTextViewProvince.text.toString()
-            viewModel.getDaftarFaskes(
-                prov,
-                autoCompleteTextViewCity.text.toString()
-            )
-            viewModel.faskesResponse.observe(viewLifecycleOwner) { response ->
-                viewModel.daftarFaskes.clear()
-                viewModel.daftarFaskes.addAll(response.data.sortedBy {
-                    distance(
-                        viewModel.latitude, viewModel.longitude,
-                        it.latitude.toDouble(), it.longitude.toDouble()
-                    )
-                }
-                    .take(5))
-                setupRecyclerView()
-            }
+//            fetchFaskes()
+        }
+
+        binding.faskesSearchButton.setOnClickListener {
+            fetchFaskes()
         }
 
         return root
@@ -131,6 +106,56 @@ class FaskesFragment : Fragment(R.layout.fragment_faskes) {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    private fun fetchProvince(response: ProvinceResponse) {
+        viewModel.provinces.addAll(response.results.map { it.key }
+            .toList())
+        val arrayAdapter = ArrayAdapter(
+            requireContext(), R.layout
+                .text_item, viewModel.provinces
+        )
+        binding.autoCompleteTextViewProvince.setAdapter(arrayAdapter)
+    }
+
+
+    private fun fetchCity() {
+        viewModel.getCity(binding.autoCompleteTextViewProvince.text.toString())
+        viewModel.cityResponse.observe(viewLifecycleOwner) { response ->
+            viewModel.cities.clear()
+            viewModel.cities.addAll(response.results.map { it.key }
+                .toList())
+            val arrayAdapter = ArrayAdapter(
+                requireContext(), R.layout
+                    .text_item, viewModel.cities
+            )
+            Log.d(
+                "Response", response.results.toString()
+            )
+            binding.autoCompleteTextViewCity.setAdapter(arrayAdapter)
+            binding.autoCompleteTextViewCity.text.clear()
+        }
+    }
+
+    private fun fetchFaskes() {
+        val prov: String =
+            binding.autoCompleteTextViewProvince.text.toString()
+        viewModel.getDaftarFaskes(
+            prov,
+            autoCompleteTextViewCity.text.toString()
+        )
+        viewModel.faskesResponse.observe(viewLifecycleOwner) { response ->
+            viewModel.daftarFaskes.clear()
+            viewModel.daftarFaskes.addAll(response.data.sortedBy {
+                distance(
+                    viewModel.latitude, viewModel.longitude,
+                    it.latitude.toDouble(), it.longitude.toDouble()
+                )
+            }
+                .take(5))
+            setupRecyclerView()
+        }
     }
 
 
@@ -213,103 +238,103 @@ class FaskesFragment : Fragment(R.layout.fragment_faskes) {
         recyclerView.setHasFixedSize(true)
     }
 
-//    private fun checkGPSPermission(): Boolean {
-//        if (activity?.let {
-//                ActivityCompat.checkSelfPermission(
-//                    it,
-//                    Manifest.permission.ACCESS_FINE_LOCATION
-//                )
-//            } == PackageManager.PERMISSION_GRANTED ||
-//            activity?.let {
-//                ActivityCompat.checkSelfPermission(
-//                    it,
-//                    Manifest.permission.ACCESS_COARSE_LOCATION
-//                )
-//            } == PackageManager.PERMISSION_GRANTED
-//        ) {
-//            return true
-//        }
-//        return false
-//    }
-//
-//    private fun requestPermission() {
-//        activity?.let {
-//            ActivityCompat.requestPermissions(
-//                it,
-//                arrayOf(
-//                    Manifest.permission.ACCESS_FINE_LOCATION,
-//                    Manifest.permission.ACCESS_COARSE_LOCATION
-//                ), 7070
-//            )
-//        }
-//    }
-//
-//    private fun isServiceEnabled(): Boolean {
-//        val locationManager = activity?.getSystemService(LOCATION_SERVICE) as
-//                LocationManager
-//        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-//            LocationManager.NETWORK_PROVIDER
-//        )
-//    }
-//
-//    @SuppressLint("MissingSuperCall")
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray
-//    ) {
-//        if (requestCode == 7070) {
-//            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Log.d("Debug:", "Permission Granted")
-//            }
-//        }
-//    }
-//
-//    private fun getLastLocation() {
-//        while (!checkGPSPermission()) {
-//            requestPermission()
-//        }
-//        if (isServiceEnabled()) {
-//            fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
-//                var location: Location? = task.result
-//                if (location != null) {
-//                    viewModel.latitude = location.latitude
-//                    viewModel.longitude = location.longitude
-//                }
-//                else {
-//                    getNewLocation()
-//                }
-//            }
-//        }
-//        else {
-//            Toast.makeText(
-//                activity, "Please enable your location service", Toast
-//                    .LENGTH_SHORT
-//            )
-//                .show()
-//        }
-//    }
-//
-//    @SuppressLint("MissingPermission")
-//    private fun getNewLocation() {
-//        val locationRequest = LocationRequest.create()
-//            .apply {
-//                interval = 100
-//                fastestInterval = 50
-//                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-//                maxWaitTime = 100
-//            }
-//        fusedLocationProviderClient!!.requestLocationUpdates(
-//            locationRequest, locationCallback, Looper.myLooper()
-//        )
-//    }
-//
-//    private val locationCallback = object : LocationCallback() {
-//        override fun onLocationResult(p0: LocationResult) {
-//            var lastLoc: Location = p0.lastLocation
-//            viewModel.latitude = lastLoc.latitude.toDouble()
-//            viewModel.longitude = lastLoc.longitude.toDouble()
-//        }
-//    }
+    private fun checkGPSPermission(): Boolean {
+        if (activity?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            } == PackageManager.PERMISSION_GRANTED ||
+            activity?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            } == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermission() {
+        activity?.let {
+            ActivityCompat.requestPermissions(
+                it,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), 7070
+            )
+        }
+    }
+
+    private fun isServiceEnabled(): Boolean {
+        val locationManager = activity?.getSystemService(LOCATION_SERVICE) as
+                LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 7070) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Debug:", "Permission Granted")
+            }
+        }
+    }
+
+    private fun getLastLocation() {
+        while (!checkGPSPermission()) {
+            requestPermission()
+        }
+        if (isServiceEnabled()) {
+            fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                var location: Location? = task.result
+                if (location != null) {
+                    viewModel.latitude = location.latitude
+                    viewModel.longitude = location.longitude
+                }
+                else {
+                    getNewLocation()
+                }
+            }
+        }
+        else {
+            Toast.makeText(
+                activity, "Please enable your location service", Toast
+                    .LENGTH_SHORT
+            )
+                .show()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getNewLocation() {
+        val locationRequest = LocationRequest.create()
+            .apply {
+                interval = 100
+                fastestInterval = 50
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                maxWaitTime = 100
+            }
+        fusedLocationProviderClient!!.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.myLooper()
+        )
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            var lastLoc: Location = p0.lastLocation
+            viewModel.latitude = lastLoc.latitude.toDouble()
+            viewModel.longitude = lastLoc.longitude.toDouble()
+        }
+    }
 
 }
