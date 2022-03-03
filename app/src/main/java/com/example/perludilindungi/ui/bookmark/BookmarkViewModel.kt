97ec75921d2860a5bddf2f91f640ff8c94453f99
mizuday <mizuday5.7@gmail.com>
2022-03-03 16:47:55 +0700

    delete unnessecary function

--- app/src/main/java/com/example/perludilindungi/ui/bookmark/BookmarkViewModel.kt
+++ app/src/main/java/com/example/perludilindungi/ui/bookmark/BookmarkViewModel.kt
@@ -1,18 +1,8 @@
 package com.example.perludilindungi.ui.bookmark
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.perludilindungi.models.Faskes
 
 class BookmarkViewModel : ViewModel() {
-
     val daftarFaskes = ArrayList<Faskes>()
-
-
-    private fun getFaskes(){
-
-    }
-
-
 }
