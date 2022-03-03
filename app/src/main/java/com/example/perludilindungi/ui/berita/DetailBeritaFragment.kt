65a5acd3e661280ba1e1ae51985b9003ad564af8
nabilahann <13519097@std.stei.itb.ac.id>
2022-03-03 08:58:33 +0700

    update url bar style

--- app/src/main/java/com/example/perludilindungi/ui/berita/DetailBeritaFragment.kt
+++ app/src/main/java/com/example/perludilindungi/ui/berita/DetailBeritaFragment.kt
@@ -7,24 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebViewClient
 import androidx.navigation.fragment.navArgs
-import com.example.perludilindungi.R
 import com.example.perludilindungi.databinding.FragmentDetailBeritaBinding
-import com.example.perludilindungi.databinding.FragmentDetailFaskesBinding
-import com.example.perludilindungi.models.Faskes
-import com.example.perludilindungi.ui.detailfaskes.DetailFaskesFragmentArgs
-import kotlinx.android.synthetic.main.activity_bottom_navigation.*
-import kotlinx.android.synthetic.main.activity_detail_berita.*
+import kotlinx.android.synthetic.main.fragment_detail_berita.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailBeritaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailBeritaFragment : Fragment() {
     val args: DetailBeritaFragmentArgs by navArgs()
     private var _binding: FragmentDetailBeritaBinding? = null
@@ -56,24 +41,4 @@ class DetailBeritaFragment : Fragment() {
         _binding = null
     }
 
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailBeritaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailBeritaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
